7f8551cf61fd4c14e80289b67a188c1a249105a9
Jamie Smith <i@jamiesmiths.com>
2018-08-13 00:23:35 -0400

    init commit, store SF uris

---
+++ ext/src/bg/README.md
@@ -0,0 +1,12 @@
+# work-watch (WIP)
+
+## Another stab at building a decent time keeper
+
+### What's that mean
+
+Very WIP but I've used & made various timers and nothing has made me happy so far 😞
+
+Building a chrome extension to auto track active time spent on SalesForce orgs / URIs to build upon and try again 😄
+
+Next, I'd like to make another VS Code extention to compliment this once data is there to combine 🤓
+
---
+++ ext/src/bg/background.js
@@ -0,0 +1,70 @@
+// jshint asi: true, esversion: 6, laxcomma: true
+
+console.log('background.js --> work-watch')
+
+/* TODOs
+
+Store all SF urls hit to start
+	L set store 
+
+UI get stored SF urls
+	L UI to set storage for job organizing	
+
+Potential blacklist ?
+	L "https://login.salesforce.com"
+	L "https://success.salesforce.com"
+	L "https://appexchange.salesforce.com"
+
+
+
+*/
+// DEV - Reset data
+//chrome.storage.sync.set({ww_store: ''})
+
+// Setup store & list of urls
+let URLs = [] // TODO I don't like this global...
+chrome.storage.sync.get(['ww_store'], (result) => {
+
+	const ww_store = result.ww_store
+
+	if(typeof ww_store !== 'object'){
+		const ww_store = {
+			urls: []
+		}
+		chrome.storage.sync.set({ww_store})
+	}
+	else {
+		URLs = ww_store.urls
+	}
+})
+
+		
+const saveUri = (uri) => {
+	chrome.storage.sync.get(['ww_store'], (result) => {
+		
+		const ww_store = result.ww_store
+
+		ww_store.urls.push(uri)
+		URLs.push(uri)
+		chrome.storage.sync.set({ww_store})
+	})
+}
+
+chrome.webRequest.onCompleted.addListener(
+	
+	(details) => {
+		if(details.url.includes('salesforce.com')){
+			
+			// Is it already in store?
+			if(URLs.includes(details.initiator)){
+				// could keep tally of hits or something... unsure if useful yet
+			}
+			else {
+				saveUri(details.initiator)
+			}
+		}
+	},
+	{urls: ["<all_urls>"]},
+	[]
+)
+
